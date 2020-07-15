afd7d3f01d7495783dc8ad90ed72175b293661e7
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-15 19:30:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,9 +11,9 @@ def loop_message_n_times(message,number)
   end
 end
 
-def output_array(message,number)
+def output_array(array)
   counter=0
-  while counter<message.length
+  while counter<array.length
     puts message[counter]
     counter+=1
   end
